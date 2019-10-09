b18750979391aa02a34a57f9627c412df3599f13
kalal31 <56273935+kalal31@users.noreply.github.com>
2019-10-09 21:23:39 +0000

    Add files via upload
    Deuxième exo

---
+++ EXO2 EVOC3.py
@@ -0,0 +1,15 @@
+######################################################################################
+#                       EVOC3 PYTHON CODING EXERCICE DE MAISON                       #
+#                         OLANLO Komlan sylvestre @kalal31                           #
+######################################################################################
+char="*"
+print(char)
+print ()
+print(char,char)
+print ()
+print(char,char,char)
+print ()
+print(char,char,char,char)
+print ()
+print(char,char,char,char,char)
+print ()
